7f770a4c3e9b655a8e34814f9a4eb0dde4789e95
wal faizy <faizy.wali@gmail.com>
2017-04-14 13:00:53 +0400

    Commit wit add functionalities

--- src/components/course/CoursePage.js
+++ src/components/course/CoursePage.js
@@ -1,11 +1,51 @@
 import React, {Component} from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../actions/courseAction";
 
-export default class CoursePage extends Component {
+ class CoursePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      course: {title: ""}
+    };
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  } //constructor
+
+ onTitleChange(event) {
+   const course = this.state.course;
+   course.title = event.target.value;
+   this.setState({course: course});
+ }
+
+ onClickSave(event) {
+   this.props.dispatch(courseActions.createCourse(this.state.course));
+   /*this.setState({course: ""});*/
+ }
+
+ courseRow(course, index) {
+   return (
+     <div key={index}> {course.title}</div>
+   );
+ }
+
   render() {
     return (
       <div>
         <h1>Course</h1>
+        {this.props.courses.map(this.courseRow)}
+        <h2>Add Course</h2>
+        <input type="text" value={this.state.course.title} onChange={this.onTitleChange} placeholder="Add course"/>
+        <input type="submit" value="save" onClick={this.onClickSave}/>
       </div>
     );
   };
 }
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToProps, null)(CoursePage);
